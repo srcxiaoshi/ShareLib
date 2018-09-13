cbd4b70385a8152a50806a406a23e1e16bd10dda
srcxiaoshi <869259080@qq.com>
2018-09-13 15:11:14 +0800

    #修改network get接口，在parameter中设置了一些默认的参数，辅助请求

--- SRCFoundation/SRCFoundation/NetWork/SRCNetworkWithAF.m
+++ SRCFoundation/SRCFoundation/NetWork/SRCNetworkWithAF.m
@@ -8,8 +8,16 @@
 
 #import "SRCNetworkWithAF.h"
 #import "AFNetworking.h"
-#import "NSString+Safe.h"
 #import "NSString+URL.h"
+
+#import "AppInfo.h"
+#import "UIDevice+Hardware.h"
+#import "UIDevice+Software.h"
+#import "NSMutableString+Safe.h"
+#import "NSMutableDictionary+Safe.h"
+#import "NSString+Safe.h"
+
+
 #import "ErrorHome.h"
 
 
@@ -70,11 +78,13 @@ static SRCNetworkWithAF *instance = nil;//单例对象
         [instance.manager.requestSerializer setValue:@"*/*" forHTTPHeaderField:@"Accept"];
         [instance.manager.requestSerializer setValue:@"keep-alive" forHTTPHeaderField:@"Connection"];
         //[instance.manager.requestSerializer setValue:@"XAUGVl9TGwYCXVFaBAk=" forHTTPHeaderField:@"X-NewRelic-ID"];
+        
+        [instance.manager.requestSerializer setValue:[NSString stringWithFormat:@"%ld",(long)[[NSDate date] timeIntervalSince1970]*1000] forHTTPHeaderField:@"tt-request-time"];
         [instance.manager.requestSerializer setValue:@"News 6.8.8 rv:6.8.8.24 (iPhone; iOS 11.4.1; en_CN) Cronet" forHTTPHeaderField:@"User-Agent"];
         [instance.manager.requestSerializer setValue:@"zh-Hans-CN;q=1, en-CN;q=0.9" forHTTPHeaderField:@"Accept-Language"];
         //[instance.manager.requestSerializer setValue:AUTHENTICATION forHTTPHeaderField:@"Authorization"];
         [instance.manager.requestSerializer setValue:@"br, gzip, deflate" forHTTPHeaderField:@"Accept-Encoding"];
-        //[instance.manager.requestSerializer setValue:@"JSESSIONID=DE1EB986168F1DEB9CEEF192061F3DE5; ngxid=fPoiPltiu8ELJylHBB5mAg==" forHTTPHeaderField:@"Cookie"];
+        //[instance.manager.requestSerializer setValue:@"Jodin_tt=03df7343018c3535a751d0c5b99dce25d3b321a46c558193caff18ea7867ae4c39eac6917b10ee913723b37258ba6a7f; install_id=43721270080; ttreq=1$d4bbc1bda6e45fbd971c8f961e25d2240be47822; alert_coverage=12" forHTTPHeaderField:@"Cookie"];
         
         
         //********************************************************
@@ -155,10 +165,54 @@ static SRCNetworkWithAF *instance = nil;//单例对象
         ERROR();
         return;
     }
+    
+  
+    //这里添加一些默认的参数，方便以后
+    NSMutableDictionary *dic=[NSMutableDictionary new];
+    [dic safe_setObject:[AppInfo app_version] forKey:@"version_code"];
+    [dic safe_setObject:[AppInfo app_build] forKey:@"tma_jssdk_version"];
+    [dic safe_setObject:[AppInfo app_name] forKey:@"app_name"];
+    [dic safe_setObject:[UIDevice deviceUUID] forKey:@"vid"];
+    [dic safe_setObject:@"47982195078" forKey:@"device_id"];
+    [dic safe_setObject:@"App Store" forKey:@"channel"];//下载渠道
+    //NSString *resolution=@"";//1125*2436//分辨率 小*大
+    [UIDevice resolutionRatio:^(float big, float little) {
+        [dic safe_setObject:[NSString stringWithFormat:@"%f*%f",little,big] forKey:@"resolution"];
+    }];
+    [dic safe_setObject:@"13" forKey:@"aid"];
+    [dic safe_setObject:@"68824" forKey:@"update_version_code"];
+    [dic safe_setObject:@"7c9b9a6b7abcbe4e79054ee4521e172ca5828555" forKey:@"openudid"];//7c9b9a6b7abcbe4e79054ee4521e172ca5828555
+    [dic safe_setObject:@"C68FFB74-9684-4105-9E8C-4954D6022577" forKey:@"idfv"];
+    [dic safe_setObject:[SRCNetworkWithAF reachAbilityString] forKey:@"ac"];//WIFI
+    [dic safe_setObject:[UIDevice deviceSystemVersion] forKey:@"os_version"];//11.4.1
+    
+    //[dic safe_setObject:@"a" forKey:@"ssmix"];
+    //这个参数比较有意思，这里给成1，则返回正常内容，否则，返回{}
+    [dic safe_setObject:@"1" forKey:@"app"];
+    
+    [dic safe_setObject:[UIDevice devicePlatform] forKey:@"device_platform"];//iPhone
+    [dic safe_setObject:@"43721270080" forKey:@"iid"];
+    //    ab_client=a1,f2,f7,e1
+    [dic safe_setObject:[UIDevice deviceModel] forKey:@"device_type"];
+    [dic safe_setObject:@"B4E58BEB-9B3D-4993-A66F-54D0E949ECF1" forKey:@"idfa"];
+    [dic safe_setObject:@"a2856219a9b65ba3a72518" forKey:@"as"];//a21560a92ec24b367a6949
     //获取时间戳
-    long recordTime = (long)[[NSDate date] timeIntervalSince1970]*1000;
-    [[[[self shareNetWorkingUtility] manager] requestSerializer] setValue:@"tt-request-time" forHTTPHeaderField:[NSString stringWithFormat:@"%ld",recordTime]];
-    [[[self shareNetWorkingUtility] manager] GET:path parameters:parameters progress:^(NSProgress * _Nonnull downloadProgress) {
+    long recordTime = (long)[[NSDate date] timeIntervalSince1970];
+    [dic safe_setObject:[NSString stringWithFormat:@"%ld",recordTime] forKey:@"ts"];//这里是时间戳
+    
+    //补充其他参数
+    NSArray *keys=[parameters allKeys];
+    for(NSString *key in keys)
+    {
+        id tmp=[parameters objectForKey:key];
+        if(tmp)
+        {
+            [dic safe_setObject:tmp forKey:key];
+        }
+    }
+    
+
+    [[[self shareNetWorkingUtility] manager] GET:path parameters:[dic copy] progress:^(NSProgress * _Nonnull downloadProgress) {
         if(downLoadProgress)
         {
             double progress=[downloadProgress fractionCompleted];
